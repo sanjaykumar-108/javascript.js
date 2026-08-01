1c18175549266368dff984e8325bd70235a1af4d
sanjaykumar-108 <sanjaykumar725685@gmail.com>
2026-08-01 18:19:10 +0000

    operation

---
+++ 01_basic/04_operation.js
@@ -0,0 +1,28 @@
+// let someNumber=33
+
+// let stringNumber=String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+//******************** operation ************************
+
+// let value=3
+
+// let negvalue=-value
+// console.log(negvalue);
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2%2);
+// string ko bhi add kar sakte hai
+let str1 ="hello"
+let str2="aman"
+
+let str3=str1+str2
+console.log(str3);
+console.log(1+"2");/isme conversion hoga/
+console.log("1"+2);// iska conversion string me hoga
+console.log("1"+2+2);// sara conversion string me ho jayega 
+// kisme conversion hona chahiye or kon si value pehle aa rahi
+//  hai ye matter karta hai
---
+++ 01_basic/05_operation.js
@@ -0,0 +1,24 @@
+
+let str1 ="hello"
+let str2="aman"
+
+// let str3=str1+str2
+// console.log(str3);
+// console.log(1+"2");/isme conversion hoga/
+// console.log("1"+2);// iska conversion string me hoga
+// console.log("1"+2+2);// sara conversion string me ho jayega 
+// kisme conversion hona chahiye or kon si value pehle aa rahi
+//  hai ye matter karta hai
+// console.log((2+4)*5%4);
+
+console.log(+true);
+console.log(+"");
+
+let num1, num2, num3;
+num1=2;
+num2=3;
+num3=4;
+let gamecounter=100;
+gamecounter++;//++ aage lagane se pehle value print hogi or ++ piche lagane se pehle value print hogi
+console.log(gamecounter);
+
