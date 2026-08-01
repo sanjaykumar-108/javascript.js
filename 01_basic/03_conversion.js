4f0b59133b8ce3644ce7815b0bc0a3e87d446fb3
sanjaykumar-108 <sanjaykumar725685@gmail.com>
2026-08-01 04:45:26 +0000

    ocnversion

---
+++ 01_basic/03_conversion.js
@@ -0,0 +1,26 @@
+let score="33abc"//double strophy lagane pe string value aane lagi
+//always valet score=nulllue zero aayegi
+//let score=undefined
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber=Number(score)//ab ye value number dikhayegi
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);//to check the tyes of datatypes
+
+
+//"33"=>33 hi aayegi
+//"33abc"=>not a number=NaN
+//true=>1; false=>0
+let isLoggedIn="aman"
+// let booleanIsLoggedIn=boolean(isLoggedIn)
+// console.log(booleanLoggedIn);
+
+//1=>true; 0=>false;
+//""=>false
+//"aman"=>true
+
+let someNumber=33
+let stringNumber=string(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
