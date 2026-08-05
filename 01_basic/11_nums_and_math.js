fbf46e003403e19b0ffe1d9116c5a94bad7e807f
sanjaykumar-108 <sanjaykumar725685@gmail.com>
2026-08-05 14:52:45 +0000

    math.num2

---
+++ 01_basic/11_nums_and_math.js
@@ -0,0 +1,18 @@
+const score=400;
+console.log(score);
+
+const balance= new Number(400);
+console.log(balance);
+
+console.log(balance.toString().length);//yeh number ke andar ka toString hai
+console.log(balance.toFixed(1));//yeh number ke andar ka toFixed hai
+const otherNumber=23.8966
+
+console.log(otherNumber.toPrecision(3));//precision=In JavaScript, precision refers to how accurately a number can be represented and stored.
+//JavaScript stores all regular numbers using the 64-bit floating-point (IEEE 754) format, which means numbers have limited precision.
+const hundreds=1000000;
+console.log(hundreds.toLocaleString('en-IN'));//yeh number ke andar ka toLocalString hai
+
+
+//++++++++++++++++++++++++++++++++maths+++++++++++++++++++++++++++++++
+
